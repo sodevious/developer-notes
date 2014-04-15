44933e54f7153ccd299c0ac2d6e755e1039131b4
Nicole Dominguez <sodevious.net@gmail.com>
2014-04-15 11:47:42 -0400

    Notes from python the hard way

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# + plus
+# - minus
+# / slash
+# * asterisk
+# % percent
+# < less than
+# > greater than
+# <= less than equal
+# >= greater than equal
+
+
+print "I will now count my chickens"
+
+print "Hens", 25 + 30 / 6
+print "Hens", 25 + 30 # 55
+print "Hens", 55 / 6 # 9
+
+print "now I will count the eggs"
+
+print 3 + 2 + 1 -6 + 4 % 2 - 1 / 4 + 6
+
+print "Is it ture that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+print "what is 3 + 2?", 3 + 2
+print "what is 5 - 7?", 5 - 7
+
+print "Oh that's why it's false"
+
+print "How about some more"
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
---
+++ python-notes.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# This is so that programmers other countries are on the same page about characters
+
+# django comes with files and some code already written
