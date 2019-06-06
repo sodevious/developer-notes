508862f05bb7f120830750946545c0c916ee2e41
Nicole Dominguez <sodevious.net@gmail.com>
2019-06-06 12:33:40 -0400

    Create hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// console.log(a); // this will return undefined
+// var a = 1;
+
+// JS splits variables in 2 in global scope and function scope
+
+var b;
+console.log(b);
+b = 2;
+
+// Functions get hoisted to the top of the scope
+
